98bcb96648315935981c093b788476c3a3d51d4a
rachmadaniHaryono <foreturiga@gmail.com>
2017-06-22 12:42:22 +0800

    new: dev: merge image model with old one.

--- rebooru/models.py
+++ rebooru/models.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
+import os
 
 from django.db import models
+from django.contrib.auth.models import User
 
 from model_utils.models import TimeStampedModel
 
 
 class Image(TimeStampedModel):
-    pass
-    
+    file = models.FileField(upload_to='images', help_text='The actual pic.', null=True, blank=True)
+    uploader = models.ForeignKey(User, help_text='The user account that uploaded the file.')
+    date_uploaded = models.DateTimeField(
+        auto_now_add=True, help_text='the time the file was uploaded.')
+    direct_url = models.URLField(help_text='direct url alternative to file.')
 
-class User(TimeStampedModel):
-    pass
-    
+    class Meta:
+        ordering = ['-date_uploaded']
+
+    def __str__(self):
+        # the original filename. eventaully make this a hash of some sort
+        try:
+            return os.path.basename(self.file.name)
+        except AttributeError:
+            return os.path.basename(self.direct_url)
+
 
 class Tag(TimeStampedModel):
     pass
-    
-
-
